8f41c86bf515145dd5ae897f320ae5cd7a2ae936
carlosg <carlosg@tid.es>
2013-10-18 11:23:27 +0200

    [MINOR] Rename build.sh by package.sh

--- package.sh
+++ package.sh
@@ -2,9 +2,9 @@
 cd sdk
 cd SOURCES
 source_file=android-sdk_r22.2.1-linux.tgz
-if [ ! -f $source_file ]; then
-   echo "Downloading 
+if [ ! -f $source_file ]; then
+   echo "Downloading $source_files"
    curl http://dl.google.com/android/$source_filse
 fi
 cd ..
-./build.sh
+$0
